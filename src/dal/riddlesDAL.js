5152461e1f3320ee39ad7e8bfb140f9476851f3c
Ariel <arieltanami122@gmail.com>
2025-07-07 21:51:31 +0300

    feat: add addRiddle to riddlesDAL
    - Implements addRiddle(riddle) to add a new riddle to the JSON file using json-file-crud
    - Returns the created riddle with id

--- src/dal/riddlesDAL.js
+++ src/dal/riddlesDAL.js
@@ -1,6 +1,6 @@
 /**
  * Riddles Data Access Layer (DAL)
- * Handles reading riddles from the JSON file using json-file-crud
+ * Handles reading and writing riddles using json-file-crud
  */
 import JsonFileCRUD from "json-file-crud";
 import { databaseConfig } from "../config/database.js";
@@ -16,6 +16,20 @@ export function getAllRiddles() {
     db.readAll((err, items) => {
       if (err) return reject(err);
       resolve(items);
+    });
+  });
+}
+
+/**
+ * Add a new riddle to the database (async/Promise)
+ * @param {Object} riddle - The riddle object to add (should have question & answer)
+ * @returns {Promise<Object>} The created riddle (with id)
+ */
+export function addRiddle(riddle) {
+  return new Promise((resolve, reject) => {
+    db.create(riddle, (err, created) => {
+      if (err) return reject(err);
+      resolve(created);
     });
   });
 }
